889d6fbb701f2a718cb4938c41e0e4159b50bb46
Gor154 <ilyeref@gmail.com>
2020-01-19 06:14:31 -0800

    added unsubscribe and multiple email functionality

--- Email_logic.py
+++ Email_logic.py
@@ -8,6 +8,7 @@ import smtplib
 import email
 import imaplib
 import mysql.connector
+import time
 
 connection = mysql.connector.connect(host='localhost', database='Hackathon', user='root', password='')
 
@@ -15,96 +16,113 @@ connection = mysql.connector.connect(host='localhost', database='Hackathon', use
 us = 'classnotifier123@gmail.com'
 passw = 'ritikaeatingfalafel'
 
-mail = imaplib.IMAP4_SSL("imap.gmail.com")
-mail.login(us,passw)
-mail.select("inbox")
+while (True):
+    try:
+        mail = imaplib.IMAP4_SSL("imap.gmail.com")
+        mail.login(us,passw)
+        mail.select("inbox")
 
-result , data = mail.uid('search', None, "ALL")
+        result , data = mail.uid('search', None, "ALL")
 
-item_list = data[0].split()
+        item_list = data[0].split()
 
-recent = item_list[-1]
-oldest = item_list[0]
-
-result2, email_data = mail.uid('fetch', recent, '(RFC822)')
-email_string = email_data[0][1].decode("utf-8")
-
-
-email_message = email.message_from_string(email_string)
-
-b = email.message_from_string(email_string)
-message = []
-
-if b.is_multipart():
-    for payload in b.get_payload():
-        message.append(payload.get_payload())
-else:
-    print(b.get_payload())
+    
+        recent = item_list[-1]
+        oldest = item_list[0]
+    
+    
     
 
-print(message[0])
-title = message[0].split()
-course = title[0]
-number = title[1]
-section = title[2]
-email = title[3]
-
-print("Course: ", course)
-print("Number: ", number)
-print("Section: ", section)
-print("Email: ", email)
-
-email_split = email.split("@")
-for i in email_split:
-    print(i)
+        result2, email_data = mail.uid('fetch', recent, '(RFC822)')
+        email_string = email_data[0][1].decode("utf-8")
 
 
+        email_message = email.message_from_string(email_string)
 
-sql_query = "INSERT INTO Mailing_list (Subject, Course_num, Section, email_1, email_2) VALUES(\'"+course+"\', \'"+number+"\', \'"+section+"\', \'"+email_split[0]+"\', \'"+email_split[1]+"\');"
+        b = email.message_from_string(email_string)
+        message = []
+        if b.is_multipart():
+            for payload in b.get_payload():
+                message.append(payload.get_payload())
+        else:
+            print(b.get_payload())
+        
+        #print(message[0])
+        title = message[0].split()
+        course = title[0]
+        number = title[1]
+        section = title[2]
+        email2 = title[3]
+
+        print("Course: ", course)
+        print("Number: ", number)
+        print("Section: ", section)
+        print("Email: ", email2)
+
+        email_split = email2.split("@")
+        # for i in email_split:
+        #     print(i)
+
+        if(course == "DELETE"):
+            sql_del_query = "DELETE FROM Mailing_list WHERE email_1=\'"+email_split[0]+"\' AND email_2 = \'"+email_split[1]+"\';"
+            cursor2 = connection.cursor()
+            result1 = cursor2.execute(sql_del_query)
+            connection.commit()
+            continue
+        
+
+        sql_query = "INSERT INTO Mailing_list (Subject, Course_num, Section, email_1, email_2) VALUES(\'"+course+"\', \'"+number+"\', \'"+section+"\', \'"+email_split[0]+"\', \'"+email_split[1]+"\');"
 
 
-cursor1 = connection.cursor()
-result = cursor1.execute(sql_query)
-connection.commit()
+        cursor1 = connection.cursor()
+        result = cursor1.execute(sql_query)
+        connection.commit()
+        typ, data = mail.search(None, 'ALL')
+        for num in data[0].split():
+            mail.store(num, '+FLAGS', '\\Deleted')
+        mail.expunge()
 
 
 
 
-link = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=" + course+"&course="+number+"&section="+section+"&campuscd=UBCO"
-r = requests.get(link)
+
+        link = "https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=" + course+"&course="+number+"&section="+section+"&campuscd=UBCO"
+        r = requests.get(link)
 
 
-soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-courseName = soup.find('title').text
+        courseName = soup.find('title').text
 
-seats = soup.find('td', text='Total Seats Remaining:').find_next_sibling('td').text
+        seats = soup.find('td', text='Total Seats Remaining:').find_next_sibling('td').text
 
-if int(seats)>0:
-    print("\nSeats available")
-else:
-    print("\nNo seats")
+        if int(seats)>0:
+            print("\nSeats available")
+        else:
+            print("\nNo seats")
 
-print(courseName)
-print(seats)
-
-
+        print(courseName)
+        print(seats)
 
 
-email = 'classnotifier123@gmail.com'
-passwd = 'ritikaeatingfalafel' 
 
-#while(1==1):
-with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
 
-    smtp.login(email,passwd)
+        email1 = 'classnotifier123@gmail.com'
+        passwd = 'ritikaeatingfalafel' 
 
-    subject = 'This program will notify your chosen class'
-    body = course+" "+number+", section "+section+" has "+seats+" seats left"
+        #while(1==1):
+        with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
 
-    msg = f'Subject: {subject}\n\n{body}'
+            smtp.login(email1,passwd)
 
-    smtp.sendmail(email,email_split[0]+"@"+email_split[1],msg)
+            subject = 'This program will notify your chosen class'
+            body = course+" "+number+", section "+section+" has "+seats+" seats left"
+
+            msg = f'Subject: {subject}\n\n{body}'
+
+            smtp.sendmail(email1,email_split[0]+"@"+email_split[1],msg)
+    except:
+        time.sleep(15)
